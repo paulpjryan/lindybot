a14661bc773c261b3afc46aa330517dd735079ff
Paul Ryan Jr. <paulpjryan@gmail.com>
2015-07-20 22:26:40 -0600

    Update lindybot.sh

--- lindybot.sh
+++ lindybot.sh
@@ -2,7 +2,10 @@
 rm -rf lindybot; true
 rm -f *.js
 
+#download the latest source
 git clone https://github.com/paulpjryan/lindybot.git
+
+#install packages and run the bot
 cd lindybot
 npm install
 node bot.js
